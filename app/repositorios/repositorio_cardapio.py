3b0eab7f358a74d132265e8ce5d75a28034d25a8
vbluuiza <luizavieirabelo3008@gmail.com>
2025-05-05 21:31:15 -0300

    Função de salvar cardapio criada

--- app/repositorios/repositorio_cardapio.py
+++ app/repositorios/repositorio_cardapio.py
@@ -11,3 +11,14 @@ def carregar_cardapio():
 
     return {}    
 
+
+def salvar_cardapio(cardapio):
+    try:
+        with open('app/dados/dados_json/cardapio.json', 'w', encoding='utf-8') as file:
+            json.dump(cardapio, file, ensure_ascii=False, indent=4)
+    except FileNotFoundError:
+        print('Erro: Arquivo não encontrado.')
+    except json.JSONDecodeError:
+        print('❌ Erro ao ler o JSON do cardápio.')
+
+    return {}    
