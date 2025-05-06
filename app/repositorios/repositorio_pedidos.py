7db44d1c460e6e735d419cf9343dda92dacbc27f
LuccaSpinelli <lbs4@cesar.scholl>
2025-05-06 15:02:50 -0300

    Repositorio pedidos: função carregar dados e salvar dados foram criadas.

--- app/repositorios/repositorio_pedidos.py
+++ app/repositorios/repositorio_pedidos.py
@@ -1,1 +1,18 @@
+import json
 
+def carregar_dados():
+    try:
+        with open('dados.json', 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        print('❌ Erro ao ler o JSON do cardápio.')    
+    return {'mesas': {}, 'pedidos': {}}
+
+def  salvar_dados(dados):
+    try:
+        with open('dados.json', 'w') as file:
+            json.dump(dados, file, indent=4)
+    except (FileNotFoundError, json.JSONDecodeError):
+        print('❌ Erro ao ler o JSON do cardápio.')
+        
+    return{}
