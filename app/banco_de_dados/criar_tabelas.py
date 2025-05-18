50e75d903b63caf002484f7519274f12ae5bfa51
Laiza <laiza.faf@hotmail.com>
2025-05-18 18:12:29 -0300

    ✨ feat: tabela pedidos e itens pedidos

--- app/banco_de_dados/criar_tabelas.py
+++ app/banco_de_dados/criar_tabelas.py
@@ -26,5 +26,30 @@ cursor.execute('''
                     
                 )
                ''')
+
+conexao.commit()
+cursor.execute('''
+                CREATE TABLE IF NOT EXISTS pedidos (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    mesa_id INTERGER NOT NULL,
+                    preco_total TEXT (10) NOT NULL,
+                    observacao TEXT (300),
+                    status TEXT(50) NOT NULL,
+                    FOREIGN KEY (mesa_id) REFERENCES mesa(id)
+                )
+               ''')
+
+conexao.commit()
+cursor.execute('''
+                CREATE TABLE IF NOT EXISTS itens_pedido (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    pedido_id INTERGER NOT NULL,
+                    cardapio_id INTERGER NOT NULL,
+                    quantidade INTEGER NOT NULL,
+                    preco_unitario TEXT (10) NOT NULL,
+                    FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
+                    FOREIGN KEY (cardapio_id) REFERENCES cardapio(id)
+                )
+               ''')
 conexao.commit()
 conexao.close()
