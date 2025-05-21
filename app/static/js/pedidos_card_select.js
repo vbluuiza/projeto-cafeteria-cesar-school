f82175600912e71577e4bac5899bed37833bb42f
LuccaSpinelli <lbs4@cesar.scholl>
2025-05-20 22:32:11 -0300

    💫: Arquivo JS pedidos_card_select.js foi criado mais código nos arquivos confirmacao_pedido.html e confirmacao_pedido.css foram criados e ajustados.

---
+++ app/static/js/pedidos_card_select.js
@@ -0,0 +1,20 @@
+// Inicio do código para seleção dos cards dos pedidos | By Lucca 
+document.addEventListener('DOMContentLoaded', function() {
+    const optionCards = document.querySelectorAll('.opicao-card');
+    optionCards.forEach(card => {
+        card.addEventListener('click', () => {
+            optionCards.forEach(c => c.classList.remove('selected'));
+            card.classList.add('selected');
+        });
+    });
+    
+    // Contador de caracteres
+    const textarea = document.querySelector('.notes-textarea');
+    const counter = document.querySelector('.character-count');
+    
+    textarea.addEventListener('input', () => {
+        const count = textarea.value.length;
+        counter.textContent = `${count}/140`;
+    });
+});
+// Fim do código para seleção dos cards dos pedidos | By Lucca 
